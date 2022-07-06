6961b8d1f02373f6460c46a528e83ead571f43fb
Lyndah Katusiime <lyndahkats@gmail.com>
2022-07-06 03:59:54 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 -  Entry point
- * Description: prints all natural numbers from n to 98, followed by a new line.
+ * Description: prints all natural numbers from n to 98, followed by a new line
  * @n: the starting point
  */
 void print_to_98(int n)
