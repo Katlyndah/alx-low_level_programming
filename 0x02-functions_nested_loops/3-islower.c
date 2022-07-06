2b3d97eefdf2153c77dd52b5eded7de4578eb87c
Lyndah Katusiime <lyndahkats@gmail.com>
2022-07-06 02:17:13 -0700

    3. Is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: checks if a character is lowercase
+ * @c: character argument
+ * Return: (1) failure (0) success
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
