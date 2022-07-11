6a966c3c07cc71f93634cf7af6f7f2632a9937b1
Lyndah Katusiime <lyndahkats@gmail.com>
2022-07-11 12:57:26 -0700

    String lemgth

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,16 +3,17 @@
 
 /**
  * _strlen -> function that returns the length of a string.
- * @s: string pointer passed to this function
- * Return: returns length of the string
+ * @s: string to count
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
-	int len;
+	int c = 0;
 
 	for (; *s != '\0'; s++)
 	{
-		len += 1;
+		c++;
 	}
+
 	return (len);
 }
