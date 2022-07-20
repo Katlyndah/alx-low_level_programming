5060adc2ddfc925ee85a7bc45637d60ecbcaec0f
Lyndah Katusiime <lyndahkats@gmail.com>
2022-07-19 18:24:03 -0700

    Task 2 fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,22 +4,21 @@
  * _strchr -> function that locates a character in a string
  * @s: string given
  * @c: another char
- * Return: a string
+ * Return: *S
  */
 char *_strchr(char *s, char c)
 {
-	int a;
-
-	while (1)
+	while (*s != '\0')
 	{
-		a = *s++;
-		if (a == c)
+		if (*s == c)
 		{
-			return (s - 1);
+			return (s);
 		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
+		s++;
 	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
 }
